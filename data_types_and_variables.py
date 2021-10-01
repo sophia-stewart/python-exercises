42f1c33b5ebe7f599d30b28f229804b3a833b675
Sophia Stewart <sophia.marcell.stewart@gmail.com>
2021-10-01 15:56:14 -0500

    Added answers and fixed formatting

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -1,45 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Data Types and Variables Exercises
+# Write some Python code, that is, variables and operators, to describe the following scenarios.
 
-# # Data Types and Variables Exercises
-# ### Write some Python code, that is, variables and operators, to describe the following scenarios.
-
-# #### You have rented some movies for your kids: 
+# You have rented some movies for your kids: 
 # - The little mermaid (for 3 days), 
 # - Brother Bear (for 5 days, they love it), and 
 # - Hercules (1 day, you don't know yet if they're going to like it). 
-# 
-# #### If price for a movie per day is 3 dollars, how much will you have to pay?
-
-# In[1]:
-
+# If price for a movie per day is 3 dollars, how much will you have to pay?
 
 price_per_day = 3
 little_mermaid = 3
 brother_bear = 5
 hercules = 1
 total_cost = price_per_day * (little_mermaid + brother_bear + hercules)
-
-
-# In[6]:
-
-
 print('The total cost will be $' + str(total_cost))
 
 
-# #### Suppose you're working as a contractor for 3 companies: 
+# Suppose you're working as a contractor for 3 companies: 
 # - Google, Amazon and Facebook, they pay you a different rate per hour. 
 # - Google pays 400 dollars per hour, 
 # - Amazon 380, and 
 # - Facebook 350. 
-# 
-# #### How much will you receive in payment for this week? 
+# How much will you receive in payment for this week? 
 # - You worked 10 hours for Facebook, 
 # - 6 hours for Google and 
 # - 4 hours for Amazon.
-
-# In[5]:
-
 
 google_wage = 400
 amazon_wage = 380
@@ -48,10 +32,8 @@ total_payment = (10 * facebook_wage) + (6 * google_wage) + (4 * amazon_wage)
 print('This week\'s payment will be $' + str(total_payment))
 
 
-# #### A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule.
-
-# In[7]:
-
+# A student can be enrolled to a class only if the class is not full 
+# and the class schedule does not conflict with her current schedule.
 
 class_is_not_full = True
 conflicting_with_schedule = False
@@ -59,10 +41,8 @@ student_can_enroll = class_is_not_full and not conflicting_with_schedule
 print(student_can_enroll)
 
 
-# #### A product offer can be applied only if people buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
-
-# In[8]:
-
+# A product offer can be applied only if people buys more than 2 items, 
+# and the offer has not expired. Premium members do not need to buy a specific amount of products.
 
 bought_more_than_2_items = False
 offer_has_not_expired = True
@@ -71,8 +51,18 @@ can_apply_product_offer = (is_a_premium_member or bought_more_than_2_items) and 
 print(can_apply_product_offer)
 
 
-# In[ ]:
+# Use the following code to follow the instructions below:
 
+username = 'codeup'
+password = 'notastrongpassword'
 
+# Create a variable that holds a boolean value for each of the following conditions:
+# the password must be at least 5 characters
+# the username must be no more than 20 characters
+# the password must not be the same as the username
+# bonus neither the username or password can start or end with whitespace
 
-
+is_at_least_5_characters = len(password) >= 5
+is_less_than_21_characters = len(password) <= 20
+is_not_same_as_username = password != username
+not_start_or_end_with_whitespace = username == username.strip() and password == password.strip()
