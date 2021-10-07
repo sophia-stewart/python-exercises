49c4d674e4852f2c07a6a0c3526924cb302e9290
Sophia Stewart <sophia.marcell.stewart@gmail.com>
2021-10-07 15:58:25 -0500

    Initial commit

---
+++ import_exericses.py
@@ -0,0 +1,9 @@
+# IMPORTS EXERCISES
+
+# 1b.
+# Create a file named import_exericses.py. 
+# Within this file, use from to import the calculate_tip function directly. 
+# Call this function with values you choose and print the result.
+
+from function_exercises import calculate_tip
+print(calculate_tip(0.2, 75))
